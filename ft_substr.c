a329836f2f7b845d324fb96612a1230319a31432
WilkPrintes <wilkp90@gmail.com>
2021-10-03 12:11:28 -0300

    update substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 17:39:34 by wprintes          #+#    #+#             */
-/*   Updated: 2021/10/03 12:05:16 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/10/03 12:10:35 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_calloc(1, 1));
 	if (s_len - start < len)
 		len = s_len - start;
-	result = malloc(len + 1);
+	result = malloc(sizeof(char) * (len + 1));
 	if(!result)
 		return (NULL);
 	while (counter < len && s[counter + start] != '\0')
