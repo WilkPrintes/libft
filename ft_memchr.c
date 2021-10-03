f4cac28459f7ab954af33154925ea1eb10b24e65
WilkPrintes <wilkp90@gmail.com>
2021-10-03 14:59:38 -0300

    update memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 00:40:20 by wprintes          #+#    #+#             */
-/*   Updated: 2021/10/02 12:57:06 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/10/03 14:59:22 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_memchr(const void *str, int c, size_t n)
 	counter = 0;
 	validation = 0;
 	str_p = (unsigned char *) str;
-	while (counter <= n)
+	while (counter < n)
 	{
 		if (*str_p == (unsigned char)c)
 		{
