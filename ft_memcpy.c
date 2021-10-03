bdc94918cfa62eb737e1eabe0557148804c6bc26
WilkPrintes <wilkp90@gmail.com>
2021-10-03 14:55:08 -0300

    update memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 00:40:28 by wprintes          #+#    #+#             */
-/*   Updated: 2021/10/03 14:52:34 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/10/03 14:55:04 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,27 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t			count;
-	unsigned char	*dest_value;
-	unsigned char	*src_value;
+	char	*dest_value;
+	char	*src_value;
 
+	src_value = (char *) src;
+	dest_value = (char *) dest;
 	count = 0;
-	while (count < n)
+	if (dest > src)
 	{
-		src_value = (unsigned char *)(src + count);
-		dest_value = (unsigned char *)(dest + count);
-		*dest_value = *src_value;
-		count++;
+		while (count < n)
+		{
+			dest_value[n - count - 1] = src_value[n - count - 1];
+			count++;
+		}
+	}
+	else if (dest < src)
+	{
+		while (count < n)
+		{
+			dest_value[count] = src_value[count];
+			count++;
+		}
 	}
 	return (dest);
 }
