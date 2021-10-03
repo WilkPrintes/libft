9a7be67a13416019ba3f262d70bec66a54e3eef4
WilkPrintes <wilkp90@gmail.com>
2021-10-03 14:33:01 -0300

    update strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 10:19:56 by wprintes          #+#    #+#             */
-/*   Updated: 2021/10/02 17:01:45 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/10/03 14:32:11 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
 	unsigned char	*str1_char;
 	unsigned char	*str2_char;
-	size_t			result;
+	int				result;
 	size_t			counter;
 
 	str1_char = (unsigned char *) str1;
@@ -25,16 +25,18 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	counter = 0;
 	if (n == 0)
 		return (0);
-	while (counter < n)
+	while (counter < n && *str1_char != '\0' && *str2_char != '\0')
 	{
 		if (*str1_char != *str2_char)
 		{
-			result = (unsigned int)*str1_char - (unsigned int)*str2_char;
+			result = *str1_char - *str2_char;
 			return (result);
 		}
 		counter++;
 		str1_char++;
 		str2_char++;
 	}
+	if (counter < n && (*str1_char == '\0' || *str2_char == '\0'))
+		result = *str1_char - *str2_char;
 	return (result);
 }
