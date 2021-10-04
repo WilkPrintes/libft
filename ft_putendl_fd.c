7cf9b3172e61548d459632c2fdc7644778a151da
WilkPrintes <wilkp90@gmail.com>
2021-10-04 20:41:45 -0300

    fix protected

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 00:03:05 by wprintes          #+#    #+#             */
-/*   Updated: 2021/10/04 20:38:07 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/10/04 20:41:28 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	if(s || fd)
+	if(s && fd)
 	{
 		ft_putstr_fd(s, fd);
 		write (fd, "\n", 1);
