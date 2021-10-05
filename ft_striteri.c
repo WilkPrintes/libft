10472f02fda5a1030fa9db2f470b337ab303df0b
WilkPrintes <wilkp90@gmail.com>
2021-10-04 21:27:25 -0300

    fix protected

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: wprintes <wilkp90@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 22:52:00 by wprintes          #+#    #+#             */
-/*   Updated: 2021/09/30 23:05:07 by wprintes         ###   ########.fr       */
+/*   Updated: 2021/10/04 21:27:00 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	size_t	counter;
 
-	counter = 0;
-	while (s[counter] != '\0')
+	if(s && f)
 	{
-		f(counter, (s + counter));
-		counter++;
+		counter = 0;
+		while (s[counter] != '\0')
+		{
+			f(counter, (s + counter));
+			counter++;
+		}
 	}
 }
